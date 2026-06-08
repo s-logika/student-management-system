98f5f279c01f5f4247a16a95e825a836584900e5
logika <logikalogeswaran@gamil.com>
2026-06-09 02:36:31 +0530

    models ingued init faile

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.course_model import Course
+from app.models.enrollment_model import Enrollment
+from app.models.lecturer_model import Lecturer
+from app.models.student_model import Student
+
+__all__ = ["Student", "Lecturer", "Course", "Enrollment"]
